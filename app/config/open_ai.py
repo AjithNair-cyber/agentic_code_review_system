beec4c208402498c2d7645455c437b767d0b8398
AjithNair-cyber <nairajith1507@gmail.com>
2026-02-03 11:48:28 +0530

    project init

---
+++ app/config/open_ai.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+from app.config.config import get_settings
+
+
+settings = get_settings()
+
+open_ai_client = ChatOpenAI(
+    api_key=settings.OPEN_AI_API_KEY,
+    model="gpt-4",
+    temperature=0.7,
+)
