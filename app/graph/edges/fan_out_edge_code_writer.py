7f2d9c4f00e3344b653489514a6aa97f92d549e1
AjithNair-cyber <nairajith1507@gmail.com>
2026-02-07 15:16:33 +0530

    added code writer agent

--- app/graph/edges/fan_out_edge_code_writer.py
+++ app/graph/edges/fan_out_edge_code_writer.py
@@ -3,6 +3,10 @@ from app.graph.state import GraphState
 
 def send_file_to_writer(state: GraphState):
     repo_data = {"repo_path" : state["repo_path"]}
+    consolidated_reviews = state.get("consolidated_reviews", [])
+    if not consolidated_reviews:
+        # Force empty review so node runs
+        return [Send("code_writer", {**repo_data, "consolidated_reviews": []})]
     return [
         Send("code_writer", {**repo_data, "consolidated_reviews": [review]})
         for review in state["consolidated_reviews"]
--- app/graph/edges/fan_out_edge_pyright_reviewer.py
+++ app/graph/edges/fan_out_edge_pyright_reviewer.py
@@ -2,6 +2,10 @@ from langgraph.types import Send
 from app.graph.state import GraphState
 
 def send_individual_pyright_error(state: GraphState):
+    pyright_errors = state.get("pyright_error_messages", [])
+    if not pyright_errors:
+        # Force empty review so node runs
+        return [Send("pyright_reviewer", {"pyright_error_messages": []})]
     return [
         Send("pyright_reviewer", {"pyright_error_messages": [error]})
         for error in state["pyright_error_messages"]
